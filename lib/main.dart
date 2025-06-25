229e78b0c5fb60fdc8e06dd8df5ec564832ffc5e
Nizar Baihaqi <nizar.albaihaqi252@gmail.com>
2025-06-25 08:01:39 +0700

    Change title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'MyBookShelf',
+      title: 'Kutubu',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.teal),
       home: const BookListScreen(),
